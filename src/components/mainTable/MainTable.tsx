7cf79b65c10f71e947e15a4d887597cfb5a4e11c
Levon Aarakelyan <fizmatstepanakert@gmail.com>
2020-04-03 15:15:53 +0400

    Added title to ToDo list

--- src/components/mainTable/MainTable.tsx
+++ src/components/mainTable/MainTable.tsx
@@ -7,7 +7,18 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
 import './MainTable.css';
+
+const Title = styled.h1`
+  font-family: sans-serif;
+  padding-left: 16px;
+`;
+
+const Border = styled.hr`
+  border: 0.5px solid #e0e0e0;
+  margin-bottom: 0px;
+`;
 
 interface Row {
   title: string;
@@ -29,29 +40,33 @@ export const MainTable:React.FC = () => {
   const columns: string[] = ['Title', 'State', 'Assigned to', 'End date'];
 
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column: string, index: number) => {
-              return <TableCell key={ index }>{ column }</TableCell>
+    <main>
+      <TableContainer component={Paper}>
+      <Title>Todos</Title>
+      <Border></Border>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column: string, index: number) => {
+                return <TableCell key={ index }>{ column }</TableCell>
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {todos.map((todo: Row, index: number) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{ todo.title }</TableCell>
+                  <TableCell>{ todo.state }</TableCell>
+                  <TableCell>{ todo.assignedTo }</TableCell>
+                  <TableCell>{ todo.deadline }</TableCell>
+                </TableRow>
+              )
             })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {todos.map((todo: Row, index: number) => {
-            return (
-              <TableRow key={index}>
-                <TableCell>{ todo.title }</TableCell>
-                <TableCell>{ todo.state }</TableCell>
-                <TableCell>{ todo.assignedTo }</TableCell>
-                <TableCell>{ todo.deadline }</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </main>
   );
 }
 
