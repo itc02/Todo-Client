f6482dd63fea734b7ae81e4c3312faaa1fd62455
Levon Aarakelyan <fizmatstepanakert@gmail.com>
2020-04-03 16:57:35 +0400

    Added button to add todos

--- src/components/mainTable/MainTable.tsx
+++ src/components/mainTable/MainTable.tsx
@@ -7,51 +7,42 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import styled from 'styled-components';
-import './MainTable.css';
-
-const Title = styled.h1`
-  font-family: sans-serif;
-  padding-left: 16px;
-`;
-
-const Border = styled.hr`
-  border: 0.5px solid #e0e0e0;
-  margin-bottom: 0px;
-`;
+import Button from '@material-ui/core/Button';
+import { Options, Title, Border } from './styles';
 
 export const MainTable:React.FC = () => {
   const todos = GetTodos();
   const columns: string[] = ['Title', 'State', 'Assigned to', 'End date'];
 
   return (
-    <main>
-      <TableContainer component={Paper}>
-      <Title>Todos</Title>
+    <TableContainer component={Paper}>
+      <Options>
+        <Title>Todos</Title>
+        <Button variant="outlined">Add</Button>
+      </Options>
       <Border></Border>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column: string) => {
-                return <TableCell key={ column }>{ column }</TableCell>
-              })}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {todos.map((todo: Data) => {
-              return (
-                <TableRow key={ todo.id }>
-                  <TableCell>{ todo.title }</TableCell>
-                  <TableCell>{ todo.state }</TableCell>
-                  <TableCell>{ todo.user_name }</TableCell>
-                  <TableCell>{ todo.deadline }</TableCell>
-                </TableRow>
-              )
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {columns.map((column: string) => {
+              return <TableCell key={ column }>{ column }</TableCell>
             })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </main>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {todos.map((todo: Data) => {
+            return (
+              <TableRow key={ todo.id }>
+                <TableCell>{ todo.title }</TableCell>
+                <TableCell>{ todo.state }</TableCell>
+                <TableCell>{ todo.user_name }</TableCell>
+                <TableCell>{ todo.deadline }</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
