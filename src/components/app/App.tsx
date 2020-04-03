c5511f7dd3ea3af8d634560e7c470e098e29292f
Levon Aarakelyan <fizmatstepanakert@gmail.com>
2020-04-03 15:15:53 +0400

    Added title to ToDo list

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
    <MainTable />
-  )
+  );
 }
 
 export default App;
--- src/components/mainTable/MainTable.tsx
+++ src/components/mainTable/MainTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetTodos } from '../../utils/requests/todos';
+import { GetTodos, Data } from '../../utils/requests/todos';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,51 +7,51 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
 import './MainTable.css';
 
-interface Row {
-  title: string;
-  state: string;
-  assignedTo: string;
-  deadline: Date;
-}
+const Title = styled.h1`
+  font-family: sans-serif;
+  padding-left: 16px;
+`;
+
+const Border = styled.hr`
+  border: 0.5px solid #e0e0e0;
+  margin-bottom: 0px;
+`;
 
 export const MainTable:React.FC = () => {
-  const todosWithAllOptions = GetTodos();
-  const todos: Row[] = todosWithAllOptions.map(todo => {
-    return {
-      title: todo.title,
-      state: todo.state,
-      assignedTo: todo.user_name,
-      deadline: todo.deadline
-    }
-  });
+  const todos = GetTodos();
   const columns: string[] = ['Title', 'State', 'Assigned to', 'End date'];
 
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column: string, index: number) => {
-              return <TableCell key={ index }>{ column }</TableCell>
+    <main>
+      <TableContainer component={Paper}>
+      <Title>Todos</Title>
+      <Border></Border>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column: string) => {
+                return <TableCell key={ column }>{ column }</TableCell>
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {todos.map((todo: Data) => {
+              return (
+                <TableRow key={ todo.id }>
+                  <TableCell>{ todo.title }</TableCell>
+                  <TableCell>{ todo.state }</TableCell>
+                  <TableCell>{ todo.user_name }</TableCell>
+                  <TableCell>{ todo.deadline }</TableCell>
+                </TableRow>
+              )
             })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {todos.map((todo: Row, index: number) => {
-            return (
-              <TableRow key={index}>
-                <TableCell>{ todo.title }</TableCell>
-                <TableCell>{ todo.state }</TableCell>
-                <TableCell>{ todo.assignedTo }</TableCell>
-                <TableCell>{ todo.deadline }</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </main>
   );
 }
 
